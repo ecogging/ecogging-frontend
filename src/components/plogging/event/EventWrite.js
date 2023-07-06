b1fb0254a09b6a319b4d8d2dc89fac3a9ecc4f31
ranopa <moonjisd5@gmail.com>
2023-07-06 18:05:35 +0900

    feat:eventList 데이터 받아오기 및 eventWrite 데이터 삽입 기능 추가

--- src/components/plogging/event/EventWrite.js
+++ src/components/plogging/event/EventWrite.js
@@ -7,10 +7,11 @@ import { reqToken } from '../../../requestToken';
 import axios from 'axios';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { useRef } from 'react';
 
 
 const EventWrite = () => {
-    const[event, setEvent] = useState({title:'', content:'',location:'',meetingDate:'',endDate:'',corpName:'',explanation:'',userId:'',fileName:''})
+    const[event, setEvent] = useState({title:'', content:'',location:'',meetingDate:'',endDate:'',corpName:'',explanation:'',userId:'', save:''})
     const [file, setFile] = useState();
     const userid = useSelector(state=>state.UserId);
     const token = useSelector(state=>state.Authorization);
@@ -26,28 +27,11 @@ const EventWrite = () => {
     // setEvent((prevEvent) => ({ ...prevEvent, content: content }));
     // };  
 
-    // const fileChange = (e) => {
-    //     //setEvent({...event,fileName:e.target.files[0].name})
-    //     setFile(e.target.files[0]);
-    // }
-
-  
-//파일 미리볼 url을 저장해줄 state
-const [fileImage, setFileImage] = useState("");
-
-// 파일 저장
-const saveFileImage = (e) => {
-    setFileImage(URL.createObjectURL(e.target.files[0]));
-};
-
-// 파일 삭제
-const deleteFileImage = () => {
-    URL.revokeObjectURL(fileImage);
-    setFileImage("");
-};
-
     const submit = (e) => {
-        e.preventDefault();
+        if (e) {
+            e.preventDefault();
+          }
+
         const formData = new FormData();
         formData.append('title', event.title);
         formData.append('content', event.content);
@@ -56,29 +40,40 @@ const deleteFileImage = () => {
         formData.append('endDate', event.endDate);
         formData.append('corpName', event.corpName);
         formData.append('explanation', event.explanation);
-        formData.append('userId', event.userId);
-        formData.append('fileName', event.fileName);
+        //formData.append('userId', event.userId);
+        formData.append('save', event.save);
+        //formData.append('fileId', event.fileId);
         formData.append('file', file);
 
         axios.post('http://localhost:8080/eventWrite', formData,
-            {
-                headers: {
-                    Authorization:token
-                },
-            })
+            // {headers: {Authorization:token},}
+            )
         .then(res=> {
             document.location.href="/eventList/1";
         })
         .catch(err=> {
-            if(err.request.status==401) {
-                console.log(err.reponse.data);
-                const rescode = err.reponse.data.rescode;
-                if(rescode==100) {
-                    reqToken(token,dispatch,cookie,setCookie,'/eventForm')
-                }
-            }
+            console.log(err);
+            // if(err.request.status==401) {
+            //     console.log(err.reponse.data);
+            //     const rescode = err.reponse.data.rescode;
+            //     if(rescode==100) {
+            //         reqToken(token,dispatch,cookie,setCookie,'/eventForm')
+            //     }
+            // }
         })
     }
+
+    const handleTempSave = (e) => {
+        e.preventDefault();
+        setEvent((prevEvent) => ({ ...prevEvent, save: 1 }));
+        submit();
+      };
+    
+      const handleRegister = (e) => {
+        e.preventDefault();
+        setEvent((prevEvent) => ({ ...prevEvent, save: 0 }));
+        submit();
+      };
 
     const modules = {
         toolbar: {
@@ -100,31 +95,51 @@ const deleteFileImage = () => {
       background-color: white;
     }`;
 
+    //파일 미리볼 url을 저장해줄 state
+    const [fileImage, setFileImage] = useState("");
+
+    // 파일 저장
+    const saveFileImage = (e) => {
+        setFileImage(URL.createObjectURL(e.target.files[0]));
+        //setEvent((prevEvent) => ({ ...prevEvent, fileId: e.target.files[0].name }));
+        setFile(e.target.files[0]);
+    };
+
+    // 파일 삭제
+    const deleteFileImage = () => {
+        URL.revokeObjectURL(fileImage);
+        fileInput.current.value = "";
+        setFileImage(null); // fileImage 초기화
+        setFile(null); // input file 초기화
+        //setEvent((prevEvent) => ({ ...prevEvent, fileId: "" })); // fileId 초기화
+    };
+
+const fileInput=useRef();
 
 
     return (
         <>
           <h1 style={{textAlign:'center', margin:'20px auto'}}>행사 기획 작성</h1>
           <div style={{margin:'0 auto',width:'100%', border:'1px solid lightgray', borderRadius:'7px', padding:'10px', backgroundColor:'rgba(243, 243, 243, 1)'}}>  
-            <Table style={{margin:'0 auto',width:'810px', padding:'20px'}}>
+            <Table style={{margin:'0 auto',width:'860px', padding:'20px'}}>
                     <tbody>
                     <tr style={{height:'10px'}}></tr>
                         <tr>
-                            <td><Label for="title">제 목</Label></td>
-                            <td style={{width:'700px'}}><Input style={{width:'700px', height:'25px'}} name="title" type="text" onChange={change}
-                                id="title" required="required" value={event.title}/></td>
+                            <td style={{width:'100px'}}><Label for="title">제 목</Label></td>
+                            <td><Input style={{width:'700px', height:'25px'}} name="title" type="text" onChange={change} id="title" required="required"
+                                 value={event.title} placeholder="제목을 입력해 주세요(20자 이하)" maxLength='20'/></td>
                         </tr>
                         <tr style={{height:'15px'}}></tr>
                         <tr>
                             <td><Label for="corpName">기업명</Label></td>
                             <td><Input style={{width:'700px', height:'25px'}} type="text" id="corpName" name="corpName" onChange={change}
-                                 required="required" value={event.corpName}/></td>
+                                 required="required" value={event.corpName} placeholder="기업명을 입력해 주세요(10자 이하)" maxLength='10'/></td>
                         </tr>
                         <tr style={{height:'15px'}}></tr>
                         <tr>
-                            <td><Label for="explanation">한 줄 소개</Label></td>
+                            <td><Label for="explanation">행사개요</Label></td>
                             <td><Input style={{width:'700px', height:'25px'}} type="text" id="explanation" name="explanation" onChange={change}
-                                 required="required" defaultValue={event.explanation}/></td>
+                                 required="required" value={event.explanation} laceholder="개요를 입력해 주세요(90자 이하)" maxLength='90'/></td>
                         </tr>
                         <tr style={{height:'15px'}}></tr>
                         <tr>
@@ -166,35 +181,37 @@ const deleteFileImage = () => {
                         </tr>
                         <tr style={{height:'15px'}}></tr>
                         <tr>
-                            <td  style={{ verticalAlign:'top'}}><Label for="content">내 용</Label></td>
+                            <td  style={{ verticalAlign:'top'}}><Label for="content">행사본문</Label></td>
                             <td>
                             <style>{qlContainerStyle}</style>
-                             <ReactQuill style={{height:'400px', background:'white'}} name='content' modules={modules} value={event.content} onChange={change}/>   
-                            {/* <Input type='textarea' id="content" name="content" onChange={change} style={{height:'100px'}}required="required" value={event.content}/> */}
+                             <ReactQuill style={{height:'400px', background:'white'}} id='content' name='content' modules={modules} value={event.content} 
+                                         onChange={(content) => {setEvent((prevEvent) => ({ ...prevEvent, content }));}}/>   
                             </td>
                         </tr>
-                        <tr style={{height:'60px'}}></tr>
+                        <tr style={{height:'70px'}}></tr>
                         <tr>
                             <td><Label for="file"> 썸네일 </Label></td>
                             <td>
                             <div>
-                                {fileImage && (<img alt="sample" src={fileImage} style={{ margin: "auto" }}/>)}
+                                {fileImage && (<img alt="sample" src={fileImage} style={{ margin: "auto", width:'285px',height:'250px'}}/>)}
                                 <div style={{alignItems: "center", justifyContent: "center"}}>
-                                <input name="file" type="file" accept="image/*" onChange={saveFileImage} style={{cursor: "pointer"}} />
+                                <input name="file" type="file" accept="image/*" ref={fileInput} onChange={saveFileImage} style={{cursor: "pointer"}} />
                                 <button style={{cursor: "pointer"}} onClick={() => deleteFileImage()}>삭제</button>
                                 </div>
                             </div>       
-                            {/* <Input name="file" type="file" id="file" accept="image/*" onChange={fileChange}/> */}
+
                             </td>
                         </tr>
-                        <tr style={{height:'15px'}}></tr>
+                        <tr style={{height:'15px'}}>
+                        <input type='hidden' name='save' id='save' value={event.save}/>
+                        </tr>
                     </tbody>
              </Table>
             </div>
             <div style={{textAlign:'center', marginTop:'30px', marginBottom:'20px'}}>
              <Button style={{boxSizing:'border-box', width:'150px', height:'33px', background:'rgba(155, 228, 206, 1)', borderRadius:'7px',fontWeight:'bold', borderStyle:'none', border:'rgba(155, 228, 206, 1) 1px solid',marginRight:'40px'}}><Link to={'/eventList'} style={{textDecoration:'none',color:'white'}}>목록</Link></Button>
-             <Button style={{boxSizing:'border-box', width:'150px', height:'33px', background:'rgba(155, 228, 206, 1)', borderRadius:'7px',fontWeight:'bold', borderStyle:'none', border:'rgba(155, 228, 206, 1) 1px solid',marginRight:'40px', color:'white'}}>임시저장</Button>
-             <Button style={{boxSizing:'border-box', width:'150px', height:'33px', background:'rgba(155, 228, 206, 1)', borderRadius:'7px',fontWeight:'bold', borderStyle:'none', border:'rgba(155, 228, 206, 1) 1px solid',marginRight:'40px', color:'white'}} onClick={submit}>등록</Button>       
+             <Button style={{boxSizing:'border-box', width:'150px', height:'33px', background:'rgba(155, 228, 206, 1)', borderRadius:'7px',fontWeight:'bold', borderStyle:'none', border:'rgba(155, 228, 206, 1) 1px solid',marginRight:'40px', color:'white'}} onClick={handleTempSave}>임시저장</Button>
+             <Button style={{boxSizing:'border-box', width:'150px', height:'33px', background:'rgba(155, 228, 206, 1)', borderRadius:'7px',fontWeight:'bold', borderStyle:'none', border:'rgba(155, 228, 206, 1) 1px solid',marginRight:'40px', color:'white'}} onClick={handleRegister}>등록</Button>       
             </div> 
         </>
     )
