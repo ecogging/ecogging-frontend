25b71d2fe4420bdf31f27e295a8a0768fda116b4
donguri <holymolyguri@gmail.com>
2023-07-08 16:53:12 +0900

    design: make MainAccompany CSS of main

--- src/components/main/MainAccompany.js
+++ src/components/main/MainAccompany.js
@@ -1,40 +1,97 @@
 import '../../styles/main/MainAccompany.css';
 import MyButton from '../common/MyButton';
 import imgTemp from '../../assets/123213.png';
-import picTemp from '../../assets/444.jpg';
+import picTemp from '../../assets/cat.png';
 import { Link } from 'react-router-dom';
 
 export default function MainAccompany() {
   return (
-    <div className='mainAccompContainer'>
-      <div className='accompBackground'>
+    <div className='container_mainAccompany'>
+      <div className='container_part_mainAccompany'>
 
-        <div className='accompTitleBox'>
-          <div className='accompTitle'>RECENT MATES</div>
+        <div className='container_title'>
+          <div className='box_title'>RECENT MATES</div>
         </div>
 
-        <div className='accompBox'>
 
-          <div className='accompCardBox'>
-            <div className='accompCard'>
 
-              <div className='cardTop-recruit'>
-                <div className='accompRecruit'>모집중</div>
+
+        <div className='container_MatesCards'>
+
+          <div className='box_MatesCard'>
+            <div className='card_MatesWhole'>
+
+              <div className='container_card_top_recruit'>
+                <div className='box_recruitState'>모집중</div>
               </div>
-              <div className='cardTop'>
-                <div className='userPic'>
-                  <img src={picTemp} className='userPicSource'/>
+              <div className='container_card_top_pic'>
+                <div className='box_pic_circle'>
+                  <img src={picTemp} className='image_userPic_source'/>
                 </div>
               </div>
 
-              <div className='cardbody'>
-                <div className='userCardNickname'>닉네임</div>
-                <Link to={"/temp"}><div className='accompCardTitle'>동행제목</div></Link>
+              <div className='container_card_middle'>
+                <div className='box_nickname'>닉네임</div>
+                <Link to={"/temp"}><div className='box_matesTitle'>동행제목</div></Link>
               </div>
 
-              <div className='cardfooter'>
-                <div className='cardMapBox'>
-                  <img src={imgTemp} alt='temp' className='cardMapImg'/>
+              <div className='container_card_bottom'>
+                <div className='box_map'>
+                  <img src={imgTemp} alt='temp' className='img_map'/>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+
+
+
+          <div className='box_MatesCard'>
+            <div className='card_MatesWhole'>
+
+              <div className='container_card_top_recruit'>
+                <div className='box_recruitState'>모집중</div>
+              </div>
+              <div className='container_card_top_pic'>
+                <div className='box_pic_circle'>
+                  <img src={picTemp} className='image_userPic_source'/>
+                </div>
+              </div>
+
+              <div className='container_card_middle'>
+                <div className='box_nickname'>닉네임</div>
+                <Link to={"/temp"}><div className='box_matesTitle'>동행제목</div></Link>
+              </div>
+
+              <div className='container_card_bottom'>
+                <div className='box_map'>
+                  <img src={imgTemp} alt='temp' className='img_map'/>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <div className='box_MatesCard'>
+            <div className='card_MatesWhole'>
+
+              <div className='container_card_top_recruit'>
+                <div className='box_recruitState'>모집중</div>
+              </div>
+              <div className='container_card_top_pic'>
+                <div className='box_pic_circle'>
+                  <img src={picTemp} className='image_userPic_source'/>
+                </div>
+              </div>
+
+              <div className='container_card_middle'>
+                <div className='box_nickname'>닉네임</div>
+                <Link to={"/temp"}><div className='box_matesTitle'>동행제목</div></Link>
+              </div>
+
+              <div className='container_card_bottom'>
+                <div className='box_map'>
+                  <img src={imgTemp} alt='temp' className='img_map'/>
                 </div>
               </div>
 
@@ -42,59 +99,17 @@ export default function MainAccompany() {
           </div>
 
 
-          {/* 2 */}
-          <div className='accompCardBox'>
-            <div className='accompCard'>
 
-              <div className='cardTop-recruit'>
-                <div className='accompRecruit'>모집중</div>
-              </div>
-              <div className='cardTop'>
-                <div className='userPic'>
-                  <img src={picTemp} className='userPicSource'/>
-                </div>
-              </div>
 
-              <div className='cardbody'>
-                <div className='userCardNickname'>닉네임</div>
-                <Link to={"/temp"}><div className='accompCardTitle'>동행제목</div></Link>
-              </div>
 
-              <div className='cardfooter'>
-                <div className='cardMapBox'>
-                  <img src={imgTemp} alt='temp' className='cardMapImg'/>
-                </div>
-              </div>
 
-            </div>
-          </div>
 
-          {/* 3 */}
-          <div className='accompCardBox'>
-            <div className='accompCard'>
 
-              <div className='cardTop-recruit'>
-                <div className='accompRecruit'>모집중</div>
-              </div>
-              <div className='cardTop'>
-                <div className='userPic'>
-                  <img src={picTemp} className='userPicSource'/>
-                </div>
-              </div>
 
-              <div className='cardbody'>
-                <div className='userCardNickname'>닉네임</div>
-                <Link to={"/temp"}><div className='accompCardTitle'>동행제목</div></Link>
-              </div>
 
-              <div className='cardfooter'>
-                <div className='cardMapBox'>
-                  <img src={imgTemp} alt='temp' className='cardMapImg'/>
-                </div>
-              </div>
 
-            </div>
-          </div>
+
+
 
 
 
--- src/components/main/MainEvents.js
+++ src/components/main/MainEvents.js
@@ -1,20 +1,39 @@
 import '../../styles/main/MainEvents.css';
+import posterTemp from '../../assets/poster.png';
 
 export default function MainEvents() {
+
+  const printTarget = (e) => {
+    alert(e.target.className);
+  }
+
   return(
-    <div className='mainEventsContainer'>
-      <div className='eventsBackground'>
-
-        <div className='eventsTitleBox'>
-          <div className='eventsTitle'>UPCOMING EVENTS</div>
+    <div className='container_mainEvents' onClick={printTarget}>
+      <div className='container_mainEvents_part'>
+        <div className='container_title'>
+          <div className='box_title'>UPCOMING EVENTS</div>
         </div>
 
-        <div className='eventsBox'>
-          <div className='eventInfo'>1</div>
-          <div className='eventInfo'>2</div>
-          <div className='eventInfo'>3</div>
-          <div className='eventInfo'>4</div>
+        {/* 카드 */}
+        <div className='container_EventsCards'>
+
+          <div className='box_EventsCards'>
+            <div className='cover_EventsCard'>
+              <div className='container_EventsCardCoverText'>
+                <div className='box_EventsCard_title'>행사 제목</div>
+                <div className='box_EventsCard_date'>행사 날짜</div>
+                <div className='box_EventsCard_writer'>행사 주관처</div>
+              </div>
+            </div>
+
+            <div className='card_EventsWhole'>
+              <img src={posterTemp} className='img_poster_source' />
+            </div>
+          
+          </div>
         </div>
+
+
       </div>
     </div>
   );
