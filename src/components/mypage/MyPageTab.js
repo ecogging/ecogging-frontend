68f88748a821d4e83c97709f472cb2660566f84e
donguri <holymolyguri@gmail.com>
2023-07-09 19:58:38 +0900

    Design: make default CSS for layout of mypage

---
+++ src/components/mypage/MyPageTab.js
@@ -0,0 +1,17 @@
+
+
+export default function MyPageTab() {
+  return(
+    <div>
+    <h1>TAB !!!!!!!</h1>
+    <h1>TAB !!!!!!!</h1>
+    <h1>TAB !!!!!!!</h1>
+    <h1>TAB !!!!!!!</h1>
+    <h1>TAB !!!!!!!</h1>
+    <h1>TAB !!!!!!!</h1>
+    <h1>TAB !!!!!!!</h1>
+    <h1>TAB !!!!!!!</h1>
+    <h1>TAB !!!!!!!</h1>
+    </div>
+  );
+}
--- src/pages/mypage/MyPage.js
+++ src/pages/mypage/MyPage.js
@@ -1,13 +1,10 @@
-
+import MyPageTab from "../../components/mypage/MyPageTab";
+import '../../styles/mypage/MyPage.css';
 
 export default function MyPage() {
   return (
-    <div>
-      <h1>MYPAGE</h1>
-      <h1>MYPAGE</h1>
-      <h1>MYPAGE</h1>
-      <h1>MYPAGE</h1>
-      <h1>MYPAGE</h1>
+    <div className="MyPage">
+      <MyPageTab />
       <h1>MYPAGE</h1>
     </div>
   );
