b52a62cc77bc9a07eed85fe40115af3343776706
jchoi <janechoi0928@gmail.com>
2023-07-10 14:44:21 +0900

    feat: 전반적인 업데이트 카드헤더에 지도 들어감, 모집임박순 추가, 새 글 작성 페이지에  date&time picker 적용, 전반적인 디자인 수정, 테스트용 데어터 넣어서 띄움

--- src/components/plogging/accompany/AccompanyList.js
+++ src/components/plogging/accompany/AccompanyList.js
@@ -1,102 +1,157 @@
 import '../../../styles/plogging/accompany/AccompanyList.css';
 import { Link } from 'react-router-dom';
 import MyButton from '../../common/MyButton';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+const { kakao } = window;
 
 const AccompanyList = () => {
+
+
+    const [page, setPage] = useState(1);
+    const [accompanys, setAccompanys] = useState([]);
+    const [hasNext, setHasNext] = useState(true);
+    const [orderby, setOrderby] = useState("createdAt");
+
+    useEffect(() => {
+        reqAccompany(page, orderby);
+    }, []);
+
+    useEffect(() => {
+        let cardHeaders = document.getElementsByClassName("card-header");
+        for(let cardHeader of cardHeaders) {
+            cardOnload(cardHeader,cardHeader.dataset.address);
+        }
+        
+    }, [accompanys]);
+
+    const changeOrderby = (porder) => {
+        reqAccompany(1,porder);
+        setOrderby(porder);
+    }    
+
+    const reqAccompany = (page, order) => {
+        axios.get(`http://localhost:8080/accompany/${page}?orderby=${order}`)
+            .then(res => {
+                console.log(res);
+                setHasNext(res.data.hasNext);
+                if(page==1) {
+                    setAccompanys([...res.data.list]);
+                } else {
+                    setAccompanys([...accompanys, ...res.data.list]);
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    const cardOnload = (e, address) => {
+        // 주소-좌표 변환 객체를 생성합니다
+        var geocoder = new kakao.maps.services.Geocoder();
+
+        geocoder.addressSearch(address, function (result, status) {
+            // 정상적으로 검색이 완료됐으면 
+            if (status === kakao.maps.services.Status.OK) {
+                let lat = result[0].y;
+                let lon = result[0].x;
+
+                // 지도 중심 위치입니다 
+                var center = new kakao.maps.LatLng(lat, lon);
+                const mapOption = {
+                    center: center, // 지도의 중심좌표
+                    level: 5 // 지도의 확대 레벨
+                };
+
+                // 지도를 생성합니다    
+                var map = new kakao.maps.Map(e, mapOption);
+                map.setZoomable(false);
+
+                // 마커를 생성합니다
+                var marker = new kakao.maps.Marker({
+                    position: center
+                });
+
+                // 마커가 지도 위에 표시되도록 설정합니다
+                marker.setMap(map);
+            }
+        });
+
+
+    };
+
+    const moreView = () => {
+        reqAccompany(page + 1,orderby);
+        setPage(page + 1);
+    }
+
+
+
+    const cardsPerRow = 3;
+
+    const splitCardsIntoRows = (cards, cardsPerRow) => {
+        const rows = [];
+        for (let i = 0; i < cards.length; i += cardsPerRow) {
+            const row = cards.slice(i, i + cardsPerRow);
+            rows.push(row);
+        }
+        return rows;
+    };
+
+    const cardRows = splitCardsIntoRows(accompanys, cardsPerRow);
+
     return (
         <div className="accompany-board">
-            <h1 className="list-subject">동행 모집</h1>
-            <table className="list-info">
-                <tbody>
-                    <tr>
-                        <td className="article-new">
-                            <Link to="/accompanywrite">
-                                <MyButton text={'새 글 작성'}></MyButton>
-                            </Link>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="list-order">
-                            <button className="list-latest">최신순 ∨</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="accompany-card">
-                            <div className="card-info">
-                                <div className="card-header">
-                                    <div className="card-progress">모집중</div>
-                                </div>
-                                <div className="card-body">
-                                    <Link to="/detail">
-                                        <p className="card-body-title">글제목</p>
-                                    </Link>
-                                    <p className="card-body-writer">작성자</p>
-                                </div>
-                                <hr/>
-                                <div className="card-footer">
-                                    <p className="plogging-date">2023-07-27</p>
-                                    <p className="participant">참여 12</p>
-                                </div>
-                            </div>
-                        
-                            <div className="card-info">
-                                <div className="card-header">
-                                    <div className="card-progress">모집중</div>
-                                </div>
-                                <div className="card-body">
-                                    <p className="card-body-title">글제목</p>
-                                    <p className="card-body-writer">작성자</p>
-                                </div>
-                                <hr/>
-                                <div className="card-footer">
-                                    <p className="plogging-date">2023-07-27</p>
-                                    <p className="participant">참여 12</p>
-                                </div>
-                            </div>
-                            <div className="card-info">
-                                <div className="card-header">
-                                    <div className="card-progress">모집중</div>
-                                </div>
-                                <div className="card-body">
-                                    <p className="card-body-title">글제목</p>
-                                    <p className="card-body-writer">작성자</p>
-                                </div>
-                                <hr/>
-                                <div className="card-footer">
-                                    <p className="plogging-date">2023-07-27</p>
-                                    <p className="participant">참여 12</p>
-                                </div>
-                            </div>                          
-
-                        </td>
-
-
-                        <td className="accompany-card">
-                            <div className="card-info">
-                                <div className="card-header">
-                                    <div className="card-progress">모집중</div>
-                                </div>
-                                <div className="card-body">
-                                    <p className="card-body-title">글제목</p>
-                                    <p className="card-body-writer">작성자</p>
-                                </div>
-                                <hr/>
-                                <div className="card-footer">
-                                    <p className="plogging-date">2023-07-27</p>
-                                    <p className="participant">참여 12</p>
-                                </div>
-                            </div>
-                        </td>
-
-
-                    </tr>  
-                    <tr>
-                        <td className="list-more">
-                            <button className="load-more">더 보기</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className="board-container">
+                <h1 className="list-subject">동행 모집</h1>
+                <table className="list-info">
+                    <tbody>
+                        <tr>
+                            <td className="article-new"> 
+                                <Link to="/accompanywrite"><MyButton text={'새 글 작성'}></MyButton></Link>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="list-order">
+                                <button className={orderby=='createdAt'? "list-latest selected": "list-latest"}  onClick={()=>changeOrderby("createdAt")}>최신순 ∨</button>&nbsp;&nbsp;&nbsp;
+                                <button className={orderby=='meetingDate'? "almost-done selected": "almost-done"} onClick={()=>changeOrderby("meetingDate")}>모집임박순 ∨</button>
+                            </td>
+                        </tr>
+                        <tr className="card-container"> 
+                            <td className="accompany-card">
+                                {cardRows.map((row, rowIndex) => (
+                                    <div key={rowIndex} className="card-row">
+                                        {row.map((card, cardIndex) => (
+                                            <div key={cardIndex} className="card-info">
+                                                <div className="card-header" data-address={card.location}></div>
+                                                <div className="card-body">
+                                                    <div>
+                                                        {card.active && <div className="card-progress">모집중</div>}
+                                                        {!card.active && <div className="progress-over">모집완료</div>}
+                                                    </div>
+                                                    <Link to="/accompanydetail" className="move-to-detail">
+                                                        <p className="card-body-title">{card.title}</p>
+                                                    </Link>
+                                                    <div className="card-body-writer">{card.nickname}</div>
+                                                </div>
+                                                <div className="card-footer">
+                                                    <div className="plogging-date">{card.meetingDate}</div>
+                                                    <div className="participant">참여 {card.joincnt}</div>
+                                                </div>
+                                            </div>
+                                        ))}
+                                    </div>
+                                ))}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="list-more">
+                                {hasNext && <button className="load-more" onClick={moreView}>더 보기</button>}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/components/plogging/accompany/AccompanyWrite.js
+++ src/components/plogging/accompany/AccompanyWrite.js
@@ -1,40 +1,94 @@
 import '../../../styles/plogging/accompany/AccompanyWrite.css';
+import MyButton from '../../common/MyButton';
+import {useState} from 'react';
+import axios from 'axios';
+import { DatePicker, TimePicker } from 'antd';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+const { daum } = window;
 
 const AccompanyWrite = () => {
+
+    const [accompany, setAccompany] = useState({title:'',content:'',numOfPeople:0, location:'',locationDetail:'',meetingDate:'',meetingTime:''});
+
+    const searchAddress = (e) => {
+        new daum.Postcode({
+            oncomplete: function(data) {
+                // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+                // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+                // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+                var addr = ''; // 주소 변수
+
+                //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+                if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                    addr = data.roadAddress;
+                } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                    addr = data.jibunAddress;
+                }
+
+                // 우편번호와 주소 정보를 해당 필드에 넣는다.
+                document.getElementById("location").value = addr;
+                setAccompany({...accompany, "location":addr});
+                // 커서를 상세주소 필드로 이동한다.
+                document.getElementById("locationDetail").focus();
+            }
+        }).open();
+    }
+
+    const changeInput = (e) => {
+        setAccompany({...accompany, [e.target.name]:e.target.value});
+    }
+
+    const accompanyWrite = (temp) => {
+        axios.post(`http://localhost:8080/accompanywrite/${temp}`,accompany)
+        .then(res=> {
+            console.log(res)
+        })
+        .catch(err=> {
+            console.log(err);
+        })
+    }
+
     return (
         <div className="accompany-new">
-            <h1 className="accompany-subject">동행 모집</h1>
-            <table className="accompany-info">
-                <tbody>
-                    <tr>
-                        <td className="accompany-cell">
-                            <input className="accompany-title" type="text" name="title" id="title" placeholder="제목"/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="accompany-cell">
-                            <input className="accompany-location" type="text" name="location" id="location" placeholder="장소 & 위치"/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="accompany-cell">
-                            <input className="accompany-headcount" type="text" name="headcount" id="headcount" placeholder="모집 인원"/>
-                            <input className="accompany-datetime" type="text" name="datetime" id="datetime" placeholder="날짜 & 시간"/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="accompany-cell">
-                            <textarea className="accompany-content" name="content" id="content" placeholder="모집글 작성"/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="accompany-buttons">
-                            <button className="accompany-save">임시 저장</button>
-                            <button className="accompany-complete">작성 완료</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className="write-container">
+                <h1 className="accompany-subject">동행 모집</h1>
+                <table className="accompany-info">
+                    <tbody>
+                        <tr>
+                            <td className="accompany-cell">
+                                <input className="accompany-title" type="text" name="title" id="title" placeholder="제목" onChange={changeInput}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="accompany-cell">
+                                <input className="accompany-loc-detail" type="text" name="locationDetail" id="locationDetail" placeholder="주소" onChange={changeInput}/>
+                                <input className="accompany-location" type="text" name="location" id="location" placeholder="장소" readOnly/>
+                                <button className="find-address" onClick={searchAddress}>주소 찾기</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="accompany-cell">
+                                <input className="accompany-headcount" type="text" name="numOfPeople" id="numOfPeople" placeholder="모집 인원" onChange={changeInput}/>
+                                <DatePicker className="accomp-date" defaultValue={dayjs('2023-01-01', 'YYYY-MM-DD') }  onChange={(date, dateString)=> {setAccompany({...accompany, meetingDate:dateString})}}/>
+                                <TimePicker className="accomp-time" defaultValue={dayjs('00-00-00', 'HH::mm::ss') }  onChange={(time, timeString)=> {setAccompany({...accompany, meetingTime:timeString})}}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="accompany-cell">
+                                <textarea className="accompany-content" name="content" id="content" placeholder="모집글 작성" onChange={changeInput}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="accompany-buttons">
+                                <MyButton text={'임시 저장'} onClick={()=>accompanyWrite(1)}></MyButton>&nbsp;&nbsp;&nbsp;
+                                <MyButton text={'작성 완료'} onClick={()=>accompanyWrite(0)}></MyButton>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
