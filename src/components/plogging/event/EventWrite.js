f88f181852a83042611dcadd0929f584eee2cf0c
ranopa <moonjisd5@gmail.com>
2023-07-10 19:20:55 +0900

    feat: evnetList 페이징 처리 완료

--- src/components/plogging/event/EventWrite.js
+++ src/components/plogging/event/EventWrite.js
@@ -71,6 +71,51 @@ const EventWrite = () => {
     
       const handleRegister = (e) => {
         e.preventDefault();
+        if (!event.title) {
+            alert('제목을 입력해 주세요');
+            return;
+          }
+        
+          if (!event.corpName) {
+            alert('기업명을 입력해 주세요');
+            return;
+          }
+        
+          if(!event.explanation){
+            alert('행사개요를 입력해 주세요');
+            return;
+          }
+                  
+          if (!event.meetingDate) {
+            alert('시작일자를 입력해 주세요');
+            return;
+          }
+        
+          if (!event.endDate) {
+            alert('종료일자를 입력해 주세요');
+            return;
+          }
+
+          
+          if (!event.location) {
+              alert('지역을 선택해 주세요');
+              return;
+            }
+            
+            if (!event.content) {
+              alert('본문을 작성해 주세요');
+              return;
+            }
+
+          if (!file) {
+            alert('썸네일을 선택해 주세요');
+            return;
+          }  
+
+          if (new Date(event.meetingDate) > new Date(event.endDate)) {
+            alert('시작일자는 종료일자와 같거나 이전 날짜여야 합니다');
+            return;
+          }
         setEvent((prevEvent) => ({ ...prevEvent, save: 0 }));
         submit();
       };
@@ -139,7 +184,7 @@ const fileInput=useRef();
                         <tr>
                             <td><Label for="explanation">행사개요</Label></td>
                             <td><Input style={{width:'700px', height:'25px'}} type="text" id="explanation" name="explanation" onChange={change}
-                                 required="required" value={event.explanation} laceholder="개요를 입력해 주세요(90자 이하)" maxLength='90'/></td>
+                                 required="required" value={event.explanation} placeholder="행사개요를 입력해 주세요(64자 이하)" maxLength='64'/></td>
                         </tr>
                         <tr style={{height:'15px'}}></tr>
                         <tr>
@@ -193,14 +238,17 @@ const fileInput=useRef();
                             <td><Label for="file"> 썸네일 </Label></td>
                             <td>
                             <div>
-                                {fileImage && (<img alt="sample" src={fileImage} style={{ margin: "auto", width:'285px',height:'250px'}}/>)}
+                                {fileImage && (<img alt="sample" src={fileImage} style={{ margin: "auto", width:'290px',height:'210px'}}/>)}
                                 <div style={{alignItems: "center", justifyContent: "center"}}>
                                 <input name="file" type="file" accept="image/*" ref={fileInput} onChange={saveFileImage} style={{cursor: "pointer"}} />
                                 <button style={{cursor: "pointer"}} onClick={() => deleteFileImage()}>삭제</button>
                                 </div>
                             </div>       
-
                             </td>
+                        </tr>
+                        <tr>
+                        <td></td>                           
+                        <td><span style={{fontSize:'14  px', color:'gray'}}>*썸네일 사이즈(290x210)</span></td>
                         </tr>
                         <tr style={{height:'15px'}}>
                         <input type='hidden' name='save' id='save' value={event.save}/>
