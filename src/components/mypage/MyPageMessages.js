cc4ba325188b7ee5f97fb65165bae5b3f9221147
donguri <holymolyguri@gmail.com>
2023-07-13 00:21:31 +0900

    Feat: connect Link to MyPage Messages 쪽지함 페이지 연결

---
+++ src/components/mypage/MyPageMessages.js
@@ -0,0 +1,14 @@
+import '../../styles/mypage/MyPageMessages.css';
+
+export default function MyPageMessages() {
+  return (
+    <div className="MyPageMessages">
+      <h1>쪽지함~!!</h1>
+      <h1>쪽지함~!!</h1>
+      <h1>쪽지함~!!</h1>
+      <h1>쪽지함~!!</h1>
+      <h1>쪽지함~!!</h1>
+      <h1>쪽지함~!!</h1>
+    </div>
+  );
+}
