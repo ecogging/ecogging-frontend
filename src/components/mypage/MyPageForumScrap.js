525290a7b388b9a0432462b5dce3dd3590af437e
donguri <holymolyguri@gmail.com>
2023-07-14 01:55:38 +0900

    Feat: make ScarpToggle of MyForumScrap 마이페이지 커뮤니티 스크랩에 스크랩 토글 버튼 CSS 추가 토글 스크랩 온오프 임시 기능 함수 추가

--- src/components/mypage/MyPageForumScrap.js
+++ src/components/mypage/MyPageForumScrap.js
@@ -1,37 +1,32 @@
 import '../../styles/mypage/MyPageForumScrap.css';
 import '../../styles/mypage/MyPageShare.css';
 import '../../styles/mypage/MyPageRecommend.css';
-import { RxBookmarkFilled, RxBookmark } from "react-icons/rx";
+import { RxBookmarkFilled } from "react-icons/rx";
 import { useState } from 'react';
 
 export default function MyPageForumScrap() {
 
-  // 검색 분류 선택 드롭다운 
-  const [isClicking, setIsClicking] = useState(false);
-  const setSearch = () => {
-    setIsClicking(!isClicking);
-  }
-
-  // 검색 분류조건 설정
-  const [isCondition, setIsCondition] = useState('전체');
-  const setCondition = (e) => {
-    setIsCondition(e.target.textContent);
+  // 스크랩 토글 - 일단 임시 (글 생성 -> 자동 인덱스, 배열 업데이트 함수 추가)
+  const [isScrapped, SetIsScrapped] = useState([true, true, true]);
+  const scrapClick = (index) => {
+    const newIsScrapped = [...isScrapped];
+    newIsScrapped[index] = !newIsScrapped[index];
+    SetIsScrapped(newIsScrapped);
   }
 
   return(
-    <div className="MyPageForumScrap" 
-      onClick={ () => { if (isClicking === true) {setSearch();} } }>
+    <div className="MyPageForumScrap">
 
       <div className='container_myForumScrapHeader'>
 
         <div className='container_myForumScrapSearch'>
           <div className='box_myScrapSearch'>
             <div className='dropbox_myScrapSearch'>
-              <div className='droplist_nowSearch' onClick={setSearch}>{isCondition}</div>
-              <ul className={isClicking ? 'droplist_myScrapSearch_clicked' : 'droplist_myScrapSearch'}>
-                <li className='drops_myScrapSearch' onClick={setCondition}>전체</li>
-                <li className='drops_myScrapSearch' onClick={setCondition}>나눔</li>
-                <li className='drops_myScrapSearch' onClick={setCondition}>추천</li>
+              <div className='droplist_nowSearch'>전체</div>
+              <ul className='droplist_myScrapSearch'>
+                <li className='drops_myScrapSearch'>전체</li>
+                <li className='drops_myScrapSearch'>나눔</li>
+                <li className='drops_myScrapSearch'>추천</li>
               </ul>
             </div>
             <input type='text' name='forumscrapSearch' className='input_myForumScrapSearch' />
@@ -56,6 +51,12 @@ export default function MyPageForumScrap() {
               <div className='container_myShareTop'>
                 <div className='container_myShareState_ongoing'>진행중</div>
                 <div className='container_myShareViews'>조회수 1004</div>
+                
+                <div className='container_myScrapToggle'
+                onClick={() => scrapClick(0)}>
+                  <RxBookmarkFilled className={isScrapped[0] ? 'icon_myScrapToggle_scrap' : 'icon_myScrapToggle_unScrap'}/>
+                </div>
+
               </div>
               <div className='container_myShareTitle'>
                 무료 나눔글 제목
@@ -84,6 +85,10 @@ export default function MyPageForumScrap() {
               <div className='container_myShareTop'>
                 <div className='container_myShareState_ongoing'>진행중</div>
                 <div className='container_myShareViews'>조회수 1004</div>
+                <div className='container_myScrapToggle'
+                onClick={() => scrapClick(1)}>
+                  <RxBookmarkFilled className={isScrapped[1] ? 'icon_myScrapToggle_scrap' : 'icon_myScrapToggle_unScrap'}/>
+                </div>
               </div>
               <div className='container_myShareTitle'>
                 무료 나눔글 제목
@@ -115,7 +120,13 @@ export default function MyPageForumScrap() {
                   경로 추천글 제목
                 </div>
                 <div className='container_myViews'>조회수 1004</div>
+
+                <div className='container_myScrapToggle'
+                onClick={() => scrapClick(2)}>
+                  <RxBookmarkFilled className={isScrapped[2] ? 'icon_myScrapToggle_scrap' : 'icon_myScrapToggle_unScrap'} id='icon_myRouteScrapToggle'/>
+                </div>
               </div>
+
               <div className='container_myRecomBottom'>
                 <div className='container_myRecomContent'>
                   본문
