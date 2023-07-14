f06d0b9d143dc5c86a6c3d9b5277907f77e89dc8
donguri <holymolyguri@gmail.com>
2023-07-14 17:57:27 +0900

    Design: make MessageReplyModal 쪽지답장모달 CSS 완료 MyButton CSS mintWide3, whiteGrayWide3 추가

--- src/components/mypage/MessageReplyModal.js
+++ src/components/mypage/MessageReplyModal.js
@@ -1,17 +1,102 @@
 import { styled } from "styled-components";
 import { useState } from "react";
+import { FaRegPaperPlane } from "react-icons/fa";
+import MyButton from "../common/MyButton";
 
 
-// const MessageReplyModal = styled(MessageReplyModal)`
-//   width:300px;
-//   height:300px;
-//   border:2px solid blue;
-// `;
+export const ModalView = styled.div`
+    width: 20vw;
+    height: 45vh;
 
-// export default function MessageReplyModal() {
-//   return(
-//     <MessageReplyModal>
-//         1
-//     </MessageReplyModal>
-//   );
-// }
+    border:1px solid #8383835b;
+    border-radius:10px;
+
+    position:fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    
+
+    display: flex;
+    flex-direction:column;
+    justify-content: center;
+    align-items: center;
+
+    background-color:white;
+
+    .replyTop {
+        width:100%;
+        height:18%;
+
+        display:flex;
+        align-items:center;
+        font-size:1.65rem;
+    }
+    .replyTitle {
+        width:140px;
+        height:50%;
+
+        display:flex;
+        align-items:center;
+        justify-content:space-between;
+        margin-left:8px;
+        color:#6CABA7;
+        font-weight:bold;
+    }
+
+    .replyMiddle {
+        width:100%;
+        height:70%;
+
+        display:flex;
+        align-items:center;
+        justify-content:center;
+    }
+    .replyContents {
+        width:93%;
+        height:95%;
+        border:none;
+        resize:none;
+        font-size:1.125rem;
+    }
+    .replyContents:focus {
+        outline:none;
+    }
+
+
+    .replyBottom {
+        width:100%;
+        height:13%;
+        margin-top:auto;
+
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        gap:6px;
+    }
+`;
+
+
+export default function MessageReplyModal({ onCloseModal }) {
+    return (
+      <>
+        <ModalView>
+            <div className="replyTop">
+                <div className="replyTitle">
+                    <FaRegPaperPlane size={'2rem'}/>
+                    답장하기
+                </div>
+            </div>
+
+            <div className="replyMiddle">
+                <textarea name="replyContents" className="replyContents" />
+            </div>
+
+            <div className="replyBottom">
+                <MyButton text={'보내기'} type={'mintWide3'}/>
+                <MyButton text={'취소'} type={'whiteGrayWide3'} onClick={onCloseModal}/>
+            </div>
+        </ModalView>
+      </>
+    );
+  }
--- src/pages/mypage/MessageDetail.js
+++ src/pages/mypage/MessageDetail.js
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 import MyButton from '../../components/common/MyButton';
 import '../../styles/mypage/MessageDetail.css';
 import MessageReplyModal from '../../components/mypage/MessageReplyModal';
@@ -7,11 +7,22 @@ import MessageReplyModal from '../../components/mypage/MessageReplyModal';
 export default function MessageDetail() {
     const goBack = useNavigate();
 
+    const [isOpen, setIsOpen] = useState(false);
+    const openReplyModal = () => {
+        setIsOpen(!isOpen);
+    }
+
+    const closeReplyModal = () => {
+        if(isOpen){
+            setIsOpen(false);
+        }
+    };
+
     return(
         <div className="MessageDetail">
-
-            {/* <MessageReplyModal /> */}
-            
+            {isOpen ? 
+                <MessageReplyModal onCloseModal={closeReplyModal} />
+                : null }
             <div className='container_messageWrapper'>
 
 
@@ -30,7 +41,7 @@ export default function MessageDetail() {
                             </div>
                             <div className='container_msgBodyHeaderRight'>
                                 <div className='container_msgBodyHeaderBtns'>
-                                    <MyButton text={'답장하기'} type={'mintWide2'}/>
+                                    <MyButton text={'답장하기'} type={'mintWide2'} onClick={openReplyModal}/>
                                     <MyButton text={'삭제하기'} type={'lightGrayWide2'}/>
                                 </div>
                             </div>
