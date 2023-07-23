3ffabd8b46f6e2077bbe72c809e2f6cb4f30ba88
donguri <holymolyguri@gmail.com>
2023-07-23 19:00:22 +0900

    Feat: MyPageMessages 쪽지함 선택 기능 구현 MyPageMessages 체크박스 추가 MyPageMessages 개별 선택 기능 추가 MyPageMessages 모두 선택 / 해제 기능 추가 완료

--- src/components/mypage/MyPageMessages.js
+++ src/components/mypage/MyPageMessages.js
@@ -10,9 +10,9 @@ export default function MyPageMessages() {
   const { userId } = useParams();
   const [msgRooms, setMegRooms] = useState([]);
 
+  // 쪽지함 데이터 불러오기
   useEffect(() => {
     const url = `/mypage/${userId}/messagerooms`;
-
     axios.get(url)
       .then((response) => {
         setMegRooms(response.data.data);
@@ -25,17 +25,35 @@ export default function MyPageMessages() {
       })
     }, [userId]); 
 
-    // 체크박스 체크 -> 선택값 변화
-    const [checkedList, setCheckedList] = useState([]);
-    const onCheckedItem = ((e)=> {
-        const nowChecked = e.target.value;
-        if(e.target.checked) {
-          setCheckedList((prevValues) => [...prevValues, nowChecked]);
-        } else {
-          setCheckedList((prevValues) => prevValues.filter((item) => item !== nowChecked));
-        }
-      });
-    console.log(checkedList);
+  // 체크박스 체크 -> 선택값 변화
+  const [checkedList, setCheckedList] = useState([]); // 선택한 값 담기는 배열
+  const [countChecked, setCountChecekd] = useState(0); // 선택한 값 갯수
+  const onCheckedItem = (checked, value) => { // 체크한 박스에 해당하는 value값, checked 상태 가져옴
+    if (checked) { // 체크 true 
+      setCheckedList((prev) => [...prev, value]); // 선택한 값 배열에 추가
+    } else {
+      setCheckedList(checkedList.filter((item) => item !== value)); // 배열에서 해당 value와 일치하지 않는 항목으로 만든 새로운 배열 반환
+    }
+  };
+  // 체크/체크해제할 때마다 카운트
+  useEffect(() => {
+    setCountChecekd(checkedList.length);
+  }, [checkedList]);
+  // 전체 선택
+  let allCheckedAr = [];
+  const setAllChecked = () => {
+    allCheckedAr = msgRooms.map((msgR) => msgR.messageRoomId+''); // 현재 모든 쪽지함id 가지는 배열 완성
+    if(countChecked === allCheckedAr.length) { // 전체 선택된 상태면 0으로 리셋
+      setCheckedList([]);
+    } else { // 전체 선택한 상태가 아니면 전체 선택
+      setCheckedList(allCheckedAr);
+    }
+  }
+
+
+
+
+
   
   return (
     <div className="MyPageMessages">
@@ -56,8 +74,12 @@ export default function MyPageMessages() {
           <div className='container_MessageOne' key={item.messageRoomId}>
             <div className='con_MessageLeft'>
               <div className='box_checkMsg'>
-                <input type="checkbox" id="check_eachMsg" name='check_eachMsg' value={item.messageRoomId} onChange={onCheckedItem}/>
+                <input type="checkbox" id={`check_eachMsg_${item.messageRoomId}`} name='check_eachMsg' value={item.messageRoomId} 
+                  onChange={(e) => {onCheckedItem(e.target.checked, e.target.value)}} 
+                  checked={ checkedList.includes(item.messageRoomId+'') ? true:false} />
               </div>
+              {console.log('======================================')}
+              {console.log(item.messageRoomId)}
               <div className='con_MessageSender'>
                 <div className='box_senderPic'></div>
                 <div className='txt_senderNick'>{item.contactNickname}</div>
@@ -85,7 +107,8 @@ export default function MyPageMessages() {
 
       <div className='container_MessageBottom'>
         <div className='container_MessageBtns'>
-          <MyButton text={'모두 선택'} type={'whiteGray'}/>
+
+          <MyButton text={'모두 선택'} type={'whiteGray'} onClick={setAllChecked} />
           <MyButton text={'삭제'} type={'gray'} />
         </div>
       </div>
