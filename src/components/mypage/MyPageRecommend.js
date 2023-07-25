df7b3f2bcbc9460f080fb2e212c572b9d5ee8537
donguri <holymolyguri@gmail.com>
2023-07-25 21:58:03 +0900

    Feat: MyPageShare 상세글 Link 누락 수정

--- src/components/mypage/MyPageRecommend.js
+++ src/components/mypage/MyPageRecommend.js
@@ -84,33 +84,32 @@ export default function MyPageRecommend() {
 
         {routes && routes.map((item) => (
 
-        <div className="container_mypageRecomWriting">
-          {/* 지도 */}
-          <div className="container_myRecomLeft" id="container_myRecomLeft" data-address={item.location} ref={(e) => mapOnload(e,item.location)}>
-            {/* <img src={temptemp}  className='temptemp'/> */}
-          </div>
+          <div className="container_mypageRecomWriting">
+            
+            <div className="container_myRecomLeft" id="container_myRecomLeft" data-address={item.location} ref={(e) => mapOnload(e,item.location)}>
+            </div>
 
-          <div className="container_myRecomRight">
-            <div className='container_myRecomWhole'>
-              <div className='container_myRecomTop'>
-                <div className='container_myRecomTitle'>
-                  {item.title}
+            <div className="container_myRecomRight">
+              <div className='container_myRecomWhole'>
+                <div className='container_myRecomTop'>
+                  <div className='container_myRecomTitle'>
+                    {item.title}
+                  </div>
+                  <div className='container_myViews'>조회수 {item.views}</div>
+                  <div className='container_myWriteDate_Recom'>{moment(item.createdAt).format('YY.MM.D h:mm a')}</div>
                 </div>
-                <div className='container_myViews'>조회수 {item.views}</div>
-                <div className='container_myWriteDate_Recom'>{moment(item.createdAt).format('YY.MM.D h:mm a')}</div>
-              </div>
-              <div className='container_myRecomBottom'>
-                <div className='container_myRecomContent'>
-                  {item.content}
-                </div>
-                <div className='container_myDetailBtns_Recom'>
-                  <div className='txt_myBtn_Recom'>수정</div>
-                  <div className='txt_myBtn_Recom'>삭제</div>
+                <div className='container_myRecomBottom'>
+                  <div className='container_myRecomContent'>
+                    {item.content}
+                  </div>
+                  <div className='container_myDetailBtns_Recom'>
+                    <div className='txt_myBtn_Recom'>수정</div>
+                    <div className='txt_myBtn_Recom'>삭제</div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
 
         ))}
 
