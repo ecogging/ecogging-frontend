9fe57c6e86013ecd016ce551044b468e59baf5e3
donguri <holymolyguri@gmail.com>
2023-07-26 15:57:04 +0900

    Feat: MyPageMessages 쪽지 읽음여부 관련 기능 완료 - 읽음여부따른 쪽지함 CSS 변경 추가 - 쪽지함 클릭하면 읽음 처리 추가

--- src/components/mypage/MyPageMessages.js
+++ src/components/mypage/MyPageMessages.js
@@ -9,9 +9,10 @@ import { getCookie } from '../../utils/CookieUtil';
 import picTemp from '../../assets/defaultProfile.PNG';
 
 export default function MyPageMessages() {
+
   const { userId } = useParams();
-  console.log('---------------------');
-  console.log(typeof(userId));
+console.log(typeof(userId));
+
   const [msgRooms, setMegRooms] = useState([]);
 
   const accessToken = getCookie('access-token'); 
@@ -29,7 +30,7 @@ export default function MyPageMessages() {
     })
       .then((response) => {
         setMegRooms(response.data.data);
-        console.log(response.data);
+        console.log(response.data.data);
       })
       .catch((err) => {
         console.log('쪽지함 목록 불러오기 실패',err);
@@ -63,25 +64,46 @@ export default function MyPageMessages() {
     }
   }
 
-  // 쪽지함 삭제
-  const url = `/mypage/${userId}/messagerooms`;
-  const deleteMsgRooms = (() => {
-    axios.delete(url, {
-      data: checkedList
-    },
-    {
-      headers:headers,
-    })
-    .then((res) => {
-      const updatedMsgRooms = msgRooms.filter((item) => !checkedList.includes(item.messageRoomId+''));
-      setMegRooms(updatedMsgRooms);
-    })
-    .catch((err) => {
-      console.log('삭제 실패');
-    });
+  // 쪽지함 삭제 ----------------------------------------------------------------------------------------
+
+    const deleteMsgRooms = (() => {
+      const url = `/mypage/${userId}/messagerooms`;
+
+      axios.delete(url, {
+        data: checkedList
+      },
+      {
+        headers:headers,
+      })
+      .then((res) => {
+        const updatedMsgRooms = msgRooms.filter((item) => !checkedList.includes(item.messageRoomId+''));
+        setMegRooms(updatedMsgRooms);
+      })
+      .catch((err) => {
+        console.log('삭제 실패');
+      });
   });
 
 
+  // 쪽지함 읽기 처리 ------------------------------------------------------------------------------------
+  
+  const [readId, setReadId] = useState(null);
+
+  const updateMsgRoomRead = (id) => {
+    setReadId(id);
+    
+    const url = `/mypage/${userId}/messagerooms/read`;
+    axios.put(url, id, { // 요청 바디에 직접 messageRoomId 값을 전달
+      headers: headers,
+    })
+    .then((res) => {
+      console.log('쪽지함 읽음 처리 완료');
+    })
+    .catch((err) => {
+      console.log('쪽지함 읽음 처리 실패');
+      console.log(err);
+    });
+  };
 
 
   
@@ -101,7 +123,18 @@ export default function MyPageMessages() {
 
       {msgRooms.map((item, idx) => (
 
-          <div className='container_MessageOne' key={item.messageRoomId}>
+      
+          <div className=
+          {           
+            (item.initialSend+"" === userId && item.readBy === 'ONLY_INITIAL_SENDER') ||
+            (item.initialRcv+"" === userId && item.readBy === 'ONLY_INITIAL_RECEIVER') ||
+            (item.readBy === 'BOTH')
+              ? 'container_MessageOne_read'
+              : 'container_MessageOne_unRead'
+          }
+
+          
+          key={item.messageRoomId}>
             <div className='con_MessageLeft'>
               <div className='box_checkMsg'>
                 <input type="checkbox" id={`check_eachMsg_${item.messageRoomId}`} name='check_eachMsg' value={item.messageRoomId} 
@@ -118,7 +151,7 @@ export default function MyPageMessages() {
               </div>
             </div>
 
-            <Link to={`/${userId}/messageroom/${item.messageRoomId}`} className='link_messageDetail'>
+            <Link to={`/${userId}/messageroom/${item.messageRoomId}`} className='link_messageDetail' onClick={() => updateMsgRoomRead(item.messageRoomId)}>
               <div className='con_MessageMiddle'>
                 { item.lastMessageContent.length > 50 ? 
                 <div className='txt_MessageContent'>{item.lastMessageContent.substring(0,50)}...</div>
